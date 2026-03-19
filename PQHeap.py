72d538cbd506997861f24dff435953681b0d199b
Ajay Kumar <ak.ajaydk2000@gmail.com>
2026-03-20 00:25:28 +0100

    Implement min-heap functions in PQHeap.py
    Added functions for heap operations including parent, left, right, and min_heapify. Also created a function to initialize an empty priority queue.

--- PQHeap.py
+++ PQHeap.py
@@ -1,1 +1,27 @@
-print("Hello World!")
+def parent(i):
+    return (i - 1) // 2
+
+def left(i):
+    return 2 * i + 1
+
+def right(i):
+    return 2 * i + 2
+
+def min_heapify(A, i):
+    l = left(i)
+    r = right(i)
+
+    if l < len(A) and A[l] < A[i]:
+        smallest = l
+    else:
+        smallest = i
+
+    if r < len(A) and A[r] < A[smallest]:
+        smallest = r
+
+    if smallest != i:
+        A[i], A[smallest] = A[smallest], A[i]
+        min_heapify(A, smallest)
+
+def createEmptyPQ():
+    return []
